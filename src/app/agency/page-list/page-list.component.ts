c6c89755fa2fe30f276334ee4b5d1bff8f16285c
Alexa <alexarieltonato@hotmail.com>
2024-09-28 14:17:41 -0500

    Se acaba el crud del page list

--- src/app/agency/page-list/page-list.component.ts
+++ src/app/agency/page-list/page-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, SimpleChanges } from '@angular/core';
 import { MatBottomSheet } from '@angular/material/bottom-sheet';
 import { MatDialog, MatDialogRef } from '@angular/material/dialog';
 import { DownloadComponent } from 'src/app/shared/download/download.component';
@@ -20,65 +20,70 @@ export interface IAgency {
   styleUrls: ['./page-list.component.css']
 })
 export class PageListComponent {
-  data: IAgency[] = [
-    { _id: 1, name: 'Ambato', address: 'Calle A' },
-    { _id: 2, name: 'Riobamba', address: 'Calle B' },
-    { _id: 3, name: 'Quito', address: 'Calle C' },
-    { _id: 4, name: 'Cuenca', address: 'Calle D' },
-    { _id: 5, name: 'Guayaquil', address: 'Calle E' },
-    { _id: 6, name: 'Ambato', address: 'Calle A' },
-    { _id: 7, name: 'Riobamba', address: 'Calle B' },
-    { _id: 8, name: 'Quito', address: 'Calle C' },
-    { _id: 9, name: 'Cuenca', address: 'Calle D' },
-    { _id: 10, name: 'Guayaquil', address: 'Calle E' },
-    { _id: 11, name: 'Ambato', address: 'Calle A' },
-    { _id: 12, name: 'Riobamba', address: 'Calle B' },
-    { _id: 13, name: 'Quito', address: 'Calle C' },
-    { _id: 14, name: 'Cuenca', address: 'Calle D' },
-    { _id: 15, name: 'Guayaquil', address: 'Calle E' } ,
-  ] 
+  data: IAgency[] = this.getDataFromLocalStorage();
 
   metaDataColumns: MetaDataColumn[] = [
     { field: "_id", title: "ID" },
     { field: "name", title: "AGENCIA" },
     { field: "address", title: "DIRECCIÓN" }
-  ]
+  ];
+
   keypadButtons: KeypadButton[] = [
     { icon: "cloud_download", tooltip: "EXPORTAR", color: "accent", action: "DOWNLOAD" },
     { icon: "add", tooltip: "AGREGAR", color: "primary", action: "NEW" }
-  ]
-  records: IAgency[] = []
-  totalRecords = this.data.length
+  ];
 
-  bottomSheet = inject(MatBottomSheet)
-  dialog = inject(MatDialog)
-  snackBar = inject(MatSnackBar)
+  records: IAgency[] = [];
+  paginatedData: IAgency[] = []; // Nuevo arreglo para los datos paginados
+  totalRecords = this.data.length;
+  currentPage = 0;  // Página actual
+
+  bottomSheet = inject(MatBottomSheet);
+  dialog = inject(MatDialog);
+  snackBar = inject(MatSnackBar);
 
   constructor() {
-    this.loadAgencies()
+    this.saveToLocalStorage();
+    this.loadAgencies();
   }
 
-
   loadAgencies() {
-    this.records = this.data
-    this.changePage(0)
+    this.records = this.data;
+    this.changePage(this.currentPage);  // Mantener la página actual
   }
 
   delete(id: number) {
-    const position = this.data.findIndex(ind => ind._id === id)
-    this.records = this.data.splice(position, 1)
-    this.loadAgencies()
-    this.saveToLocalStorage()
-    this.showMessage('Registro eliminado')
+    const position = this.data.findIndex(ind => ind._id === id);
+    this.data.splice(position, 1);  // Eliminar el registro
+    this.totalRecords = this.data.length;
+    this.saveToLocalStorage();
+    this.loadAgencies();  // Mantener la página actual
+    this.showMessage('Registro eliminado');
   }
 
-  getDataFromLocalStorage(){
-    const agencies = localStorage.getItem('agencies')
-    return agencies ? JSON.parse(agencies) : []
+  getDataFromLocalStorage() {
+    const agencies = localStorage.getItem('agencies');
+    return agencies ? JSON.parse(agencies) : [
+      { _id: 1, name: 'Ambato', address: 'Calle A' },
+      { _id: 2, name: 'Riobamba', address: 'Calle B' },
+      { _id: 3, name: 'Quito', address: 'Calle C' },
+      { _id: 4, name: 'Cuenca', address: 'Calle D' },
+      { _id: 5, name: 'Guayaquil', address: 'Calle E' },
+      { _id: 6, name: 'Ambato', address: 'Calle A' },
+      { _id: 7, name: 'Riobamba', address: 'Calle B' },
+      { _id: 8, name: 'Quito', address: 'Calle C' },
+      { _id: 9, name: 'Cuenca', address: 'Calle D' },
+      { _id: 10, name: 'Guayaquil', address: 'Calle E' },
+      { _id: 11, name: 'Ambato', address: 'Calle A' },
+      { _id: 12, name: 'Riobamba', address: 'Calle B' },
+      { _id: 13, name: 'Quito', address: 'Calle C' },
+      { _id: 14, name: 'Cuenca', address: 'Calle D' },
+      { _id: 15, name: 'Guayaquil', address: 'Calle E' },
+    ];
   }
 
-  saveToLocalStorage(){
-    localStorage.setItem('agencies', JSON.stringify(this.data))
+  saveToLocalStorage() {
+    localStorage.setItem('agencies', JSON.stringify(this.data));
   }
 
   openForm(row: any | null = null) {
@@ -86,52 +91,52 @@ export class PageListComponent {
       panelClass: 'panel-container',
       disableClose: true,
       data: row
-    }
-    const reference: MatDialogRef<FormComponent> = this.dialog.open(FormComponent, options)
+    };
+    const reference: MatDialogRef<FormComponent> = this.dialog.open(FormComponent, options);
 
     reference.afterClosed().subscribe((response) => {
-      if (!response) { return }
+      if (!response) { return; }
       if (response._id) {
-        const agencia = { ...response }
-        this.data = this.data.map(ind => ind._id === agencia._id ? agencia : ind)
-        this.loadAgencies()
-        this.saveToLocalStorage()
-        this.showMessage('Registro actualizado')
+        const agencia = { ...response };
+        this.data = this.data.map(ind => ind._id === agencia._id ? agencia : ind);
+        this.saveToLocalStorage();
+        this.loadAgencies();  // Mantener la página actual
+        this.showMessage('Registro actualizado');
       } else {
-        const lastId = this.data[this.data.length - 1]._id
-        const agencia = { ...response, _id: lastId + 1 }
-        this.data.push(agencia)
-        this.loadAgencies()
-        this.saveToLocalStorage()
-        this.showMessage('Registro exitoso')
+        const lastId = this.data[this.data.length - 1]._id;
+        const agencia = { ...response, _id: lastId + 1 };
+        this.data.push(agencia);
+        this.totalRecords = this.data.length;
+        this.saveToLocalStorage();
+        this.loadAgencies();  // Mantener la página actual
+        this.showMessage('Registro exitoso');
       }
-    })
+    });
   }
-
 
   doAction(action: string) {
     switch (action) {
       case 'DOWNLOAD':
-        this.showBottomSheet("Lista de Agencias", "agencias", this.data)
-        break
+        this.showBottomSheet("Lista de Agencias", "agencias", this.data);
+        break;
       case 'NEW':
-        this.openForm()
-        break
+        this.openForm();
+        break;
     }
   }
 
   showBottomSheet(title: string, fileName: string, data: any) {
-    this.bottomSheet.open(DownloadComponent)
+    this.bottomSheet.open(DownloadComponent);
   }
 
   showMessage(message: string, duration: number = 3000) {
-    this.snackBar.open(message, '', { duration })
+    this.snackBar.open(message, '', { duration });
   }
 
   changePage(page: number) {
-    const pageSize = environment.PAGE_SIZE
-    const skip = pageSize * page
-    this.data = this.records.slice(skip, skip + pageSize)
+    this.currentPage = page;  // Actualizar la página actual
+    const pageSize = environment.PAGE_SIZE;
+    const skip = pageSize * page;
+    this.paginatedData = this.records.slice(skip, skip + pageSize); // Actualiza solo los datos paginados
   }
-
 }
