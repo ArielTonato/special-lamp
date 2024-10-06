ebdba6120a3429a77ca44c2bd51746b7cbbce684
Ari <alexarieltonato@hotmail.com>
2024-10-06 11:54:01 -0500

    Se agrega el channel service

--- src/app/channel/services/channel.service.ts
+++ src/app/channel/services/channel.service.ts
@@ -17,9 +17,24 @@ export class ChannelService {
     return this.http.get<ICanal[]>(this.apiUrl);
   }
 
+  //Obtener un canal por ID
+  getChannelById(id: number):Observable<ICanal> {
+    return this.http.get<ICanal>(`${this.apiUrl}/${id}`);
+  }
+
   //Guardar un canal
   saveChannel(channel: ICanal):Observable<ICanal> {
     return this.http.post<ICanal>(this.apiUrl, channel);
+  }
+
+  //Actualizar un canal
+  updateChannel(channel: ICanal):Observable<void> {
+    return this.http.put<void>(`${this.apiUrl}/${channel._id}`, channel);
+  }
+
+  //Eliminar un canal
+  deleteChannel(id: number):Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
   }
 
 }
